59b4c92d07c6bd2f1f3a806d9e45546ab814dacc
Charles Milette <charles.milette@gmail.com>
2019-12-05 00:16:21 -0500

    Remove frame pointer on x86 stack thunk
    Fixes #8

--- include/member_thunk/x86/x86_stack_thunk.hpp
+++ include/member_thunk/x86/x86_stack_thunk.hpp
@@ -9,38 +9,29 @@ namespace member_thunk
 {
 	MEMBER_THUNK_PACK
 
-	// push ebp
-	// mov ebp, esp
 	// mov eax, {function}
-	// mov DWORD PTR [ebp+8], {this}
-	// pop ebp
+	// mov dword ptr [esp+4], {this}
 	// jmp eax
 	template<typename Func>
 		requires is_this_on_stack_v<Func>
 	class thunk<Func, architecture::x86> final : public crtp_thunk<thunk<Func, architecture::x86>, Func>
 	{
-		std::uint8_t push_ebp;
-		std::uint8_t mov_ebp_esp[2];
 		std::uint8_t mov_eax;
 		void* function;
-		std::uint8_t mov_ebp_plus_8[3];
+		std::uint8_t mov_esp_plus_4[4];
 		void* that;
-		std::uint8_t pop_ebp;
 		std::uint8_t jmp_eax[2];
 
 	public:
 		template<typename Class, typename MemberFunc>
 		thunk(Class* pThis, MemberFunc pFunc) :
-			push_ebp(0x55),
-			mov_ebp_esp { 0x8b, 0xEC },
 			mov_eax(0xB8),
 			function(reinterpret_cast<void*&>(pFunc)),
-			mov_ebp_plus_8 { 0xC7, 0x45, 0x08 },
+			mov_esp_plus_4 { 0xC7, 0x44, 0x24, 0x04 },
 			that(pThis),
-			pop_ebp(0x5d),
 			jmp_eax { 0xFF, 0xE0 }
 		{
-			MEMBER_THUNK_STATIC_ASSERT_SIZEOF_THIS(18);
+			MEMBER_THUNK_STATIC_ASSERT_SIZEOF_THIS(15);
 
 			this->flush();
 		}
