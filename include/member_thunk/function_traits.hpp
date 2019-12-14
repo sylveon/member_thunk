dbfc4bbbe2d15fbf62a46e64e125e48714d97a00
Charles Milette <charles.milette@gmail.com>
2019-12-13 23:19:36 -0500

    Add support for pseudo type deduction from left hand side and noexcept function pointers, add void or scalar return check

--- include/member_thunk/function_traits.hpp
+++ include/member_thunk/function_traits.hpp
@@ -12,26 +12,32 @@ namespace member_thunk
 	inline constexpr bool is_compatible_function_types_v< \
 		Ret(convention*)(FirstArg, Args...), \
 		Class, Ret(convention Class::*)(Args...) specifiers \
-	> = sizeof(FirstArg) == sizeof(void*) && std::is_trivially_destructible_v<FirstArg>
+	> = sizeof(FirstArg) == sizeof(void*) && std::is_trivial_v<FirstArg> && \
+		(std::is_void_v<Ret> || std::is_scalar_v<Ret>);
 
-#define MEMBER_THUNK_GENERATE_CALLING_CONVENTION_SPECIALIZATION(convention) \
-	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, ); \
-	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, const);
+#define MEMBER_THUNK_GENERATE_NOEXCEPT_SPECIALIZATION(convention, specifiers) \
+	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, specifiers); \
+	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, specifiers noexcept);
+
+#define MEMBER_THUNK_GENERATE_CONST_SPECIALIZATION(convention) \
+	MEMBER_THUNK_GENERATE_NOEXCEPT_SPECIALIZATION(convention, ); \
+	MEMBER_THUNK_GENERATE_NOEXCEPT_SPECIALIZATION(convention, const);
 
 	// Specifying any calling convention on a function pointer in non-x86 will
 	// explicitely specify the regular calling convention for that platform.
 	// /Gv would make __vectorcall implicit on regular pointers in x64.
-	MEMBER_THUNK_GENERATE_CALLING_CONVENTION_SPECIALIZATION(__stdcall)
+	MEMBER_THUNK_GENERATE_CONST_SPECIALIZATION(__stdcall);
 #if defined(_M_AMD64) || defined(_M_IX86)
 	// Same than normal x64 calling convention/x86 __fastcall but with vector
 	// registers used to pass vector parameters.
-	MEMBER_THUNK_GENERATE_CALLING_CONVENTION_SPECIALIZATION(__vectorcall)
+	MEMBER_THUNK_GENERATE_CONST_SPECIALIZATION(__vectorcall);
 #endif
 #if defined(_M_IX86)
-	MEMBER_THUNK_GENERATE_CALLING_CONVENTION_SPECIALIZATION(__cdecl)
-	MEMBER_THUNK_GENERATE_CALLING_CONVENTION_SPECIALIZATION(__fastcall)
+	MEMBER_THUNK_GENERATE_CONST_SPECIALIZATION(__cdecl);
+	MEMBER_THUNK_GENERATE_CONST_SPECIALIZATION(__fastcall);
 #endif
 
-#undef MEMBER_THUNK_GENERATE_CALLING_CONVENTION_SPECIALIZATION
+#undef MEMBER_THUNK_GENERATE_CONST_SPECIALIZATION
+#undef MEMBER_THUNK_GENERATE_NOEXCEPT_SPECIALIZATION
 #undef MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION
 }
--- include/member_thunk/member_thunk.hpp
+++ include/member_thunk/member_thunk.hpp
@@ -12,6 +12,26 @@ namespace member_thunk
 	{
 		return std::make_unique<thunk<Func>>(pThis, pFunc);
 	}
+
+	template<typename Class, typename MemberFunc>
+	struct factory
+	{
+		Class* pThis;
+		MemberFunc pFunc;
+
+		template<typename Func>
+			requires is_compatible_function_types_v<Func, Class, MemberFunc>
+		operator std::unique_ptr<thunk<Func>>()
+		{
+			return make<Func>(pThis, pFunc);
+		}
+	};
+
+	template<typename Class, typename MemberFunc>
+	factory<Class, MemberFunc> make(Class* pThis, MemberFunc pFunc)
+	{
+		return { pThis, pFunc };
+	}
 }
 
 #if defined(_M_IX86)
