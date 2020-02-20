b30a4b6173e84a601b5926c048a851b64254849d
Charles Milette <charles.milette@gmail.com>
2020-02-20 11:52:07 -0500

    Add support for noexcept function pointers
    Fixes #23

--- include/member_thunk/details/function_traits.hpp
+++ include/member_thunk/details/function_traits.hpp
@@ -18,19 +18,23 @@ namespace member_thunk::details
 	template<typename MemberFunc>
 	using get_this_ptr_t = std::add_pointer_t<typename get_this<MemberFunc>::type>;
 
-#define MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers) \
+#define MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers, ptr_specifiers) \
 	template<typename Ret, typename Class, typename FirstArg, typename... Args> \
 	inline constexpr bool is_compatible_function_types_v< \
-		Ret(convention*)(FirstArg, Args...), \
+		Ret(convention*)(FirstArg, Args...) ptr_specifiers, \
 		Ret(convention Class::*)(Args...) specifiers \
-	> = is_pointer_like_v<FirstArg> && is_scalar_or_void_v<Ret>; \
-	 \
+	> = is_pointer_like_v<FirstArg> && is_scalar_or_void_v<Ret>;
+
+#define MEMBER_THUNK_GENERATE_THIS_SPECIALIZATION(convention, inheritance, specifiers) \
 	template<typename Ret, typename Class, typename... Args> \
 	struct get_this<Ret(convention Class::*)(Args...) specifiers> : inheritance<Class> { };
 
 #define MEMBER_THUNK_GENERATE_NOEXCEPT_SPECIALIZATION(convention, inheritance, specifiers) \
-	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers) \
-	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers noexcept)
+	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers, ) \
+	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers noexcept, ) \
+	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers noexcept, noexcept) \
+	MEMBER_THUNK_GENERATE_THIS_SPECIALIZATION(convention, inheritance, specifiers) \
+	MEMBER_THUNK_GENERATE_THIS_SPECIALIZATION(convention, inheritance, specifiers noexcept)
 
 #define MEMBER_THUNK_GENERATE_CONST_SPECIALIZATION(convention) \
 	MEMBER_THUNK_GENERATE_NOEXCEPT_SPECIALIZATION(convention, std::type_identity, ) \
@@ -52,5 +56,6 @@ namespace member_thunk::details
 
 #undef MEMBER_THUNK_GENERATE_CONST_SPECIALIZATION
 #undef MEMBER_THUNK_GENERATE_NOEXCEPT_SPECIALIZATION
+#undef MEMBER_THUNK_GENERATE_THIS_SPECIALIZATION
 #undef MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION
 }
