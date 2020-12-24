418c5db17093a32a2cd74877110ecb79ededda77
Charles Milette <charles.milette@gmail.com>
2020-12-24 16:10:50 -0500

    Remove inheritance from MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION

--- include/member_thunk/details/function_traits.hpp
+++ include/member_thunk/details/function_traits.hpp
@@ -4,7 +4,7 @@
 namespace member_thunk::details
 {
 	template<typename T>
-	inline constexpr bool is_pointer_like_v = std::is_pointer_v<T> || (std::is_integral_v<T> && sizeof(T) == sizeof(void*));
+	inline constexpr bool is_pointer_like_v = std::is_scalar_v<T> && !std::is_floating_point_v<T> && sizeof(T) == sizeof(void*);
 
 	template<typename T>
 	inline constexpr bool is_scalar_or_void_v = std::is_void_v<T> || std::is_scalar_v<T>;
@@ -19,7 +19,7 @@ namespace member_thunk::details
 	using get_this_ptr_t = std::add_pointer_t<typename get_this<MemberFunc>::type>;
 
 	// clang-format off
-#define MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers, ptr_specifiers) \
+#define MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, specifiers, ptr_specifiers) \
 	template<typename Ret, typename Class, typename FirstArg, typename... Args> \
 	inline constexpr bool is_compatible_function_types_v< \
 		Ret(convention*)(FirstArg, Args...) ptr_specifiers, \
@@ -32,9 +32,9 @@ namespace member_thunk::details
 	// clang-format on
 
 #define MEMBER_THUNK_GENERATE_NOEXCEPT_SPECIALIZATION(convention, inheritance, specifiers) \
-	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers, ) \
-	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers noexcept, ) \
-	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, inheritance, specifiers noexcept, noexcept) \
+	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, specifiers, ) \
+	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, specifiers noexcept, ) \
+	MEMBER_THUNK_GENERATE_MEMBER_SPECIALIZATION(convention, specifiers noexcept, noexcept) \
 	MEMBER_THUNK_GENERATE_THIS_SPECIALIZATION(convention, inheritance, specifiers) \
 	MEMBER_THUNK_GENERATE_THIS_SPECIALIZATION(convention, inheritance, specifiers noexcept)
 
